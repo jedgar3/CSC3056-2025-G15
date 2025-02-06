6a7b932be4a5eb590d98ac30f282a591df2af380
Morgan Mcdonald <morgano1312@gmail.com>
2025-02-06 12:48:04 +0000

    Added Assertions to TransactionTest

--- Lab1/src/tests/TransactionTest.java
+++ Lab1/src/tests/TransactionTest.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 public class TransactionTest {
     public static void main(String[] args) {
         testTransactionConstructorBySelection();
+        TestTransactionConstructorByAssert();
     }
 
     public static void testTransactionConstructorBySelection() {
@@ -46,6 +47,23 @@ public class TransactionTest {
         } else {
             TestUtils.printTestFailed(testCaseName);
         }
+    }
+
+    public static void TestTransactionConstructorByAssert(){
+        // 1 - Setup
+        String testTransactionAccount = "1";
+        double testTransactionAmount = 100.42;
+        Date testTransactionDate = new Date();
+
+        // 2 - Exercise
+        Transaction testTransaction = new Transaction(testTransactionAccount, testTransactionDate, testTransactionAmount);
+
+        // 3 - Verify
+        System.out.println("Starting the assertion of test method: testTransactionConstructorBySelection");
+
+        assert Objects.equals(testTransaction.getAccountNum(), testTransactionAccount) : "Test failed";
+        assert Objects.equals(testTransaction.getTransactionAmount(), testTransactionAmount) : "Test failed";
+        assert Objects.equals(testTransaction.getTransactionDate(), testTransactionDate) : "Test failed";
     }
 
 }
